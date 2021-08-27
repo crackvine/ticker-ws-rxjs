848ff1872eb17c95549ec89ff3a1e17081421227
crackvine <crackvine@parsley>
2021-08-27 08:03:36 +0200

    Basic setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { interval } from 'rxjs';
+import { map, filter, takeWhile } from 'rxjs/operators';
+
+// An OBSERVER which logs
+const loggerObserver = {
+  next: (x) => {
+    console.log(x);
+  },
+  complete: () => { console.log('done'); },
+  error: (error) => { console.error(error); },
+};
+
+// An interval OBSERVABLE
+const ticker$ = interval(2000).pipe(
+  takeWhile((x) => x < 5),
+  map((x) => x * 2 + x),
+  filter((x) => x % 2 === 0),
+);
+
+// A SUBSCRIPTION of the logger observer to the interval observable
+const loggerSub1 = ticker$.subscribe(loggerObserver);
+
+// UNSUBSCRIBING the subscription after a period of time
+setTimeout(() => { loggerSub1.unsubscribe(); }, 20000);
+
