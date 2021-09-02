888f53345da93bae0e19cb41774905dc925965a4
crackvine <crackvine@parsley>
2021-09-02 08:28:35 +0200

    add d3

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
 import { webSocket } from 'rxjs/webSocket';
 import { map, scan, filter } from 'rxjs/operators';
+import * as d3 from 'd3';
 
-const OVER = 60;
-const subject = webSocket('wss://stream.binance.com:9443/ws/btcusdt@ticker');
+const SYMBOL = 'BTCUSDT';
+const AVG_OVER = 60;
 
-const mapped = subject.pipe(
+const svg = d3.select('#plot1');
+
+const subject$ = webSocket(`wss://stream.binance.com:9443/ws/${SYMBOL.toLowerCase()}@ticker`);
+
+const rollingAvg$ = subject$.pipe(
   scan((acc, curr) => {
     acc.push(curr.a);
-    if (acc.length > OVER) { acc.shift(); }
+    if (acc.length > AVG_OVER) {
+      acc.shift();
+    }
     return acc;
   }, []),
-  map(arr => arr.reduce((avg, val) => avg + parseFloat(val), 0) / arr.length),
+  map((arr) => arr.reduce((avg, val) => avg + parseFloat(val), 0) / arr.length),
 );
 
-mapped.subscribe({
-  next: msg => console.log(msg), 
-  error: err => console.log(err), 
-  complete: () => console.log('complete')
+const sub1 = rollingAvg$.subscribe({
+  next: (msg) => console.log('Rolling average: ' + msg),
+  error: (err) => console.log(err),
+  complete: () => console.log('complete'),
 });
+
+const subA = subject$.subscribe({
+  next: (msg) => plotDataPoint(msg.a),
+  error: (err) => console.log('ERROR: ' + err),
+});
+
+// const subB = subject$.subscribe({
+//   next: (msg) => console.log('SubB: ' + msg.a),
+//   error: (err) => console.log('ERROR: ' + err),
+// });
+
+let x = 0;
+const plotDataPoint = (val) => {
+  console.log('plotting value ', val);
+  const y = d3.scaleLinear().domain([49500, 50000]).range([0, 400]);
+  console.log(y(val));
+  svg.append('circle').attr('cx', x).attr('cy', y(val)).attr('r', 2).style('fill', 'blue');
+  x++;
+};
